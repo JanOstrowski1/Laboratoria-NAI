7d0e0e32c68274a62aff5b64aeeb72992fdfceb5
Jan Ostrowski <s23820@pjwstk.edu.pl>
2022-10-05 16:20:08 +0200

    fin

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,36 +1,28 @@
 #include <iostream>
-#include <any>
-#include <functional>
-#include <map>
 #include <string>
 #include <vector>
-
-using mojamapa_t = std::map<std::string, std::string>;
-using mojafunkcja_t = std::function<std::string(std::string)>;
-
-void wypisz(mojamapa_t mapa, mojafunkcja_t fun) {
-    using namespace std;
-    for (auto kv: mapa) {
-        auto [k, v] = kv;
-        cout << "klucz: " << k << "; wartosc " << fun(v) << endl;
-    }
-}
-
-
+#include <cmath>
+#define PI 3.14159265
 int main(int argc, char **argv) {
     using namespace std;
-    map<string, string> mapa = {{"imie", "Jan"}};
-    map<string, mojafunkcja_t> formatery;
-    formatery["r"] = [](string x) { return "== " + x + " =="; };
-    formatery["p"] = [](string x) { return "__" + x + "__"; };
-    try {
-        vector<string> argumenty(argv, argv + argc);
-        auto selected_f = argumenty.at(1);
-        wypisz(mapa, formatery.at(selected_f));
-    } catch (std::out_of_range aor) {
-        cout << "podaj argument. Dostepne to: ";
-        for (auto [k, v]: formatery) cout << " " << k;
-        cout << endl;
+    vector<string> argumenty(argv, argv + argc);
+
+    string func_name= argumenty.at(2);
+
+    if(func_name=="sin"){
+        float first_arg= stof(argumenty.at(3));
+        cout<< sin (first_arg*PI/180);
+    }else if(func_name=="add"){
+        float first_arg= stof(argumenty.at(3));
+        float second_arg= stof(argumenty.at(4));
+        cout<< (first_arg+ second_arg);
+    }else if(func_name=="mod"){
+        int first_arg= stoi(argumenty.at(3));
+        int second_arg= stoi(argumenty.at(4));
+        cout<< (first_arg % second_arg);
+    }else{
+        return 1;
     }
+
     return 0;
 }
