da75a196d8f496becfcff7560899e46e3aec9d2f
Jan Ostrowski <s23820@pjwstk.edu.pl>
2022-10-12 15:58:36 +0200

    lab2

--- lab1/cmake-build-debug/CMakeFiles/lab1.dir/DependInfo.cmake
+++ lab1/cmake-build-debug/CMakeFiles/lab1.dir/DependInfo.cmake
@@ -1,15 +1,14 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/mnt/c/Users/janos/Programowanie/semestr5/Laboratoria-NAI/lab1/main.cpp" "/mnt/c/Users/janos/Programowanie/semestr5/Laboratoria-NAI/lab1/cmake-build-debug/CMakeFiles/lab1.dir/main.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/s23820/Laboratoria-NAI/lab1/main.cpp" "CMakeFiles/lab1.dir/main.cpp.obj" "gcc" "CMakeFiles/lab1.dir/main.cpp.obj.d"
   )
 
 # Targets to which this target links.
--- lab1/cmake-build-debug/CMakeFiles/lab1.dir/cmake_clean.cmake
+++ lab1/cmake-build-debug/CMakeFiles/lab1.dir/cmake_clean.cmake
@@ -1,7 +1,10 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/lab1.dir/main.cpp.o"
-  "lab1"
+  "CMakeFiles/lab1.dir/main.cpp.obj"
+  "CMakeFiles/lab1.dir/main.cpp.obj.d"
+  "lab1.exe"
+  "lab1.exe.manifest"
   "lab1.pdb"
+  "liblab1.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
