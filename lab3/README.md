0d7cd26ded5df1bffa2adcd6a6f6dc62af901512
JanOstrowski1 <jan.ostrowski1337@gmail.com>
2022-11-02 12:00:56 +0100

    lab 3 fin

---
+++ lab3/README.md
@@ -0,0 +1,66 @@
+Brute Force
+-----------------------------------------------------------------------------------
+Best Booth x = 3.24504 y = 1.20493 | result: 9.07237 | duration: 4434
+
+Best Booth x = 1.00013 y = 3 | result: 8.68204e-08 | duration: 11256
+
+Best Booth x = 1.00044 y = 3 | result: 9.65194e-07 | duration: 8600
+
+Best Holder table x = 8.0551 y = 9.664 | result: -19.2085 | duration: 17241
+
+Best Holder table x = 8.05522 y = 9.664 | result: -19.2085 | duration: 3113
+
+Best Holder table x = 8.05568 y = 9.664 | result: -19.2085 | duration: 19621
+
+Best Tree Hump Camel x = 0.000284805 y = 4.10089e-15 | result: 1.62228e-07 | duration: 18766
+
+Best Tree Hump Camel x = -0.000312967 y = 4.10089e-15 | result: 1.95896e-07 | duration: 12950
+
+Best Tree Hump Camel x = -0.000438599 y = 4.10089e-15 | result: 3.84738e-07 | duration: 19308
+
+
+Hill Climbing
+-----------------------------------------------------------------------------------
+Best Booth x = 0.999982 y = 3.00002 | result: 7.22335e-10 | duration: 26
+
+Best Booth x = 1 y = 3 | result: 1.84532e-10 | duration: 25
+
+Best Booth x = 0.999994 y = 2.99999 | result: 6.69001e-10 | duration: 24
+
+Best Holder table x = 4.90114 y = -6.53252 | result: -4.71246 | duration: 34
+
+Best Holder table x = -8.05503 y = 9.66459 | result: -19.2085 | duration: 33
+
+Best Holder table x = 11.2926 y = -3.22888 | result: -14.7318 | duration: 34
+
+Best Tree Hump Camel x = -1.74755 y = 0.873776 | result: 0.298638 | duration: 32
+
+Best Tree Hump Camel x = 1.74756 y = -0.873775 | result: 0.298638 | duration: 31
+
+Best Tree Hump Camel x = -1.73955e-06 y = 5.17219e-06 | result: 2.38063e-11 | duration: 33
+
+Simulated Annealing
+-----------------------------------------------------------------------------------
+Best Booth x = 0.930981 y = 3.03865 | result: 0.009946 | duration: 41
+
+Best Booth x = 1.02463 y = 2.93316 | result: 0.0122011 | duration: 41
+
+Best Booth x = 0.90921 y = 3.21135 | result: 0.111057 | duration: 40
+
+Best Holder table x = -8.00432 y = -9.76757 | result: -19.0761 | duration: 64
+
+Best Holder table x = 8.0307 y = -9.62151 | result: -19.1839 | duration: 69
+
+Best Holder table x = 8.14027 y = -9.45015 | result: -18.6962 | duration: 65
+
+Best Tree Hump Camel x = -0.0749326 y = 0.385247 | result: 0.130744 | duration: 57
+
+Best Tree Hump Camel x = 0.185083 y = 0.0809095 | result: 0.0888074 | duration: 56
+
+Best Tree Hump Camel x = 0.0123207 y = -0.191202 | result: 0.034506 | duration: 57
+
+-----------------------------------------------------------------------------------
+Na podstawie wyników eksperymentu stwierdzam że algorytm Brute Force osiąga najlepsze wyniki, czas jego trwania jest najwolniejszy.
+-----------------------------------------------------------------------------------
+Najgorsze wyniki w moich eksperymentach osiągnął Hill Climbing.
+-----------------------------------------------------------------------------------
--- lab3/main.cpp
+++ lab3/main.cpp
@@ -12,52 +12,55 @@ auto coords_generator = [](double minR, double maxR) {
     return std::pair<double, double>(coords(mt_generator), coords(mt_generator));
 };
 
-auto brute_force = [](auto f, auto domain, int iterations, double minR, double maxR) {
-    auto current_p = domain(minR, maxR);
+auto brute_force = [](auto f, int iterations, double minimal_d, double maximal_d) {
+    std::pair<double, double> current_p = coords_generator(minimal_d, maximal_d);
     auto best_point = current_p;
+    double precision = (maximal_d - minimal_d) / (iterations/10);
 
-    for (int i = 0; i < iterations; ++i) {
-        if (f(current_p) < f(best_point)) {
-            best_point = current_p;
+    while (current_p.first < maximal_d) {
+        while (current_p.second < maximal_d) {
+            if (f(current_p) < f(best_point)) {
+                best_point = current_p;
+            }
+            current_p.second += precision;
         }
-        current_p = domain(minR, maxR);
+        current_p.first += precision;
+        current_p.second = minimal_d + precision;
     }
 
     return best_point;
 };
 
-auto hill_climbing = [](auto f, auto domain, int max_iterations, double minimal_d, double maximal_d) {
-    std::pair<double, double> current_p = domain(minimal_d, maximal_d);
+auto hill_climbing = [](auto f, int iterations, double minimal_d, double maximal_d) {
+    std::pair<double, double> current_p = coords_generator(minimal_d, maximal_d);
 
-    for (int iteration = 0; iteration < max_iterations; iteration++) {
+    for (int iteration = 0; iteration < iterations; iteration++) {
         std::pair<double, double> new_p;
-        std::pair<double, double> neighbour_p = domain(minimal_d / 100, maximal_d / 100);
-        new_p.first = current_p.first + neighbour_p.first;
-        new_p.second = current_p.second + neighbour_p.second;
+        std::pair<double, double> close_p = coords_generator(-1.0/200, 1.0/200);
+        new_p.first = current_p.first + close_p.first;
+        new_p.second = current_p.second + close_p.second;
+
         if (f(current_p) > f(new_p)) {
             current_p = new_p;
         }
     }
     return current_p;
-};
+    }
+;
 
-auto simulated_annealing = [](auto f, auto domain, int max_iterations, double minimal_d, double maximal_d) {
+auto simulated_annealing = [](auto f, int max_iterations, double minimal_d, double maximal_d) {
 
-    std::vector<std::pair<double, double>> pairsVector;
     std::uniform_real_distribution<double> uk_dis(0, 1);
     double uk = uk_dis(mt_generator);
 
-    auto best_point = domain(minimal_d, maximal_d);
-    pairsVector.push_back(best_point);
+    auto best_point = coords_generator(minimal_d, maximal_d);
 
     for (int i = 0; i < max_iterations; ++i) {
-        auto tk = domain(minimal_d, maximal_d);
+        auto tk = coords_generator(minimal_d, maximal_d);
         if (f(tk) <= f(best_point)) {
             best_point = tk;
-            pairsVector.push_back(best_point);
         } else if (uk < exp(-(std::abs(f(tk) - f(best_point)) / (1 / log(i))))) {
             best_point = tk;
-            pairsVector.push_back(best_point);
         }
     }
     return best_point;
@@ -73,13 +76,13 @@ auto printResults = [](auto algorithm, const std::string &algorithmName,
     std::cout << "-----------------------------------------------------------------------------------" << std::endl;
     for (int i = 0; i < 3; i++) {
         auto start = std::chrono::high_resolution_clock::now();
-        auto best_point = algorithm(function, coords_generator, iterations,
+        auto best_point = algorithm(function, iterations,
                                     domain_range.first, domain_range.second);
         auto stop = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
         std::cout << "Best " << functionName << " x = " << best_point.first << " y = " << best_point.second
                   << " | result: " << function(best_point) << " | duration: "
-                  << duration << std::endl;
+                  << duration <<"[ms]"<< std::endl;
         std::cout << "-----------------------------------------------------------------------------------" << std::endl;
     }
 };
@@ -105,18 +108,18 @@ int main() {
 //    std::vector<std::pair<double, double>> domain_range = {{-10, 10},{-10, 10},{-5,  5}};
 //    std::vector<std::string> algorithmsNames = {"Brute Force", "Hill Climbing", "Simulated Annealing"};
 
-
+//
     printResults(brute_force, "Brute Force", boothFunc, "Booth", {-10, 10}, 100000);
     printResults(brute_force, "Brute Force", holderTableFunc, "Holder table", {-10, 10}, 100000);
     printResults(brute_force, "Brute Force", treeHumpCamelFunc, "Tree Hump Camel", {-5, 5}, 100000);
 
-    std::cout<<std::endl;
+    std::cout << std::endl;
 
     printResults(hill_climbing, "Hill Climbing", boothFunc, "Booth", {-10, 10}, 100000);
     printResults(hill_climbing, "Hill Climbing", holderTableFunc, "Holder table", {-10, 10}, 100000);
     printResults(hill_climbing, "Hill Climbing", treeHumpCamelFunc, "Tree Hump Camel", {-5, 5}, 100000);
 
-    std::cout<<std::endl;
+    std::cout << std::endl;
 
     printResults(simulated_annealing, "Simulated Annealing", boothFunc, "Booth", {-10, 10}, 100000);
     printResults(simulated_annealing, "Simulated Annealing", holderTableFunc, "Holder table", {-10, 10}, 100000);
