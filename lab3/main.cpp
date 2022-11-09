8553b8b7e2f4e33b2d4b1b2979c4fd72d2e3903b
JanOstrowski1 <jan.ostrowski1337@gmail.com>
2022-11-09 20:27:15 +0100

    bug fix

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -15,7 +15,7 @@ auto coords_generator = [](double minR, double maxR) {
 auto brute_force = [](auto f, int iterations, double minimal_d, double maximal_d) {
     std::pair<double, double> current_p = coords_generator(minimal_d, maximal_d);
     auto best_point = current_p;
-    double precision = (maximal_d - minimal_d) / (iterations/10);
+    double precision = (maximal_d - minimal_d) / (iterations);
 
     while (current_p.first < maximal_d) {
         while (current_p.second < maximal_d) {
@@ -36,7 +36,7 @@ auto hill_climbing = [](auto f, int iterations, double minimal_d, double maximal
 
     for (int iteration = 0; iteration < iterations; iteration++) {
         std::pair<double, double> new_p;
-        std::pair<double, double> close_p = coords_generator(-1.0/200, 1.0/200);
+        std::pair<double, double> close_p = coords_generator(-1.0 / 200, 1.0 / 200);
         new_p.first = current_p.first + close_p.first;
         new_p.second = current_p.second + close_p.second;
 
@@ -45,18 +45,22 @@ auto hill_climbing = [](auto f, int iterations, double minimal_d, double maximal
         }
     }
     return current_p;
-    }
-;
+};
 
 auto simulated_annealing = [](auto f, int max_iterations, double minimal_d, double maximal_d) {
 
     std::uniform_real_distribution<double> uk_dis(0, 1);
-    double uk = uk_dis(mt_generator);
+
 
     auto best_point = coords_generator(minimal_d, maximal_d);
 
     for (int i = 0; i < max_iterations; ++i) {
-        auto tk = coords_generator(minimal_d, maximal_d);
+
+        double uk = uk_dis(mt_generator);
+        std::normal_distribution<> neighbour;
+        std::pair<double, double> tk;
+        tk.first= best_point.first+ neighbour(mt_generator) * 0.01;
+        tk.second= best_point.second+ neighbour(mt_generator) * 0.01;
         if (f(tk) <= f(best_point)) {
             best_point = tk;
         } else if (uk < exp(-(std::abs(f(tk) - f(best_point)) / (1 / log(i))))) {
@@ -82,7 +86,7 @@ auto printResults = [](auto algorithm, const std::string &algorithmName,
         auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
         std::cout << "Best " << functionName << " x = " << best_point.first << " y = " << best_point.second
                   << " | result: " << function(best_point) << " | duration: "
-                  << duration <<"[ms]"<< std::endl;
+                  << duration << "[ms]" << std::endl;
         std::cout << "-----------------------------------------------------------------------------------" << std::endl;
     }
 };
@@ -102,28 +106,22 @@ int main() {
         double x = pair.first, y = pair.second;
         return 2 * pow(x, 2) - 1.05 * pow(x, 4) + pow(x, 6) / 6 + x * y + pow(y, 2);
     };
-    std::vector<std::function<double(std::pair<double, double> pair)>> functions = {boothFunc, holderTableFunc,
-                                                                                    treeHumpCamelFunc};
-//    std::vector<std::string> functionNames = {"Booth", "Holder table", "Tree Hump Camel"};
-//    std::vector<std::pair<double, double>> domain_range = {{-10, 10},{-10, 10},{-5,  5}};
-//    std::vector<std::string> algorithmsNames = {"Brute Force", "Hill Climbing", "Simulated Annealing"};
 
-//
-    printResults(brute_force, "Brute Force", boothFunc, "Booth", {-10, 10}, 100000);
-    printResults(brute_force, "Brute Force", holderTableFunc, "Holder table", {-10, 10}, 100000);
-    printResults(brute_force, "Brute Force", treeHumpCamelFunc, "Tree Hump Camel", {-5, 5}, 100000);
+    printResults(brute_force, "Brute Force", boothFunc, "Booth", {-10, 10}, 10000);
+    printResults(brute_force, "Brute Force", holderTableFunc, "Holder table", {-10, 10}, 10000);
+    printResults(brute_force, "Brute Force", treeHumpCamelFunc, "Tree Hump Camel", {-5, 5}, 10000);
 
     std::cout << std::endl;
 
-    printResults(hill_climbing, "Hill Climbing", boothFunc, "Booth", {-10, 10}, 100000);
-    printResults(hill_climbing, "Hill Climbing", holderTableFunc, "Holder table", {-10, 10}, 100000);
-    printResults(hill_climbing, "Hill Climbing", treeHumpCamelFunc, "Tree Hump Camel", {-5, 5}, 100000);
+    printResults(hill_climbing, "Hill Climbing", boothFunc, "Booth", {-10, 10}, 1000000);
+    printResults(hill_climbing, "Hill Climbing", holderTableFunc, "Holder table", {-10, 10}, 1000000);
+    printResults(hill_climbing, "Hill Climbing", treeHumpCamelFunc, "Tree Hump Camel", {-5, 5}, 1000000);
 
     std::cout << std::endl;
 
-    printResults(simulated_annealing, "Simulated Annealing", boothFunc, "Booth", {-10, 10}, 100000);
-    printResults(simulated_annealing, "Simulated Annealing", holderTableFunc, "Holder table", {-10, 10}, 100000);
-    printResults(simulated_annealing, "Simulated Annealing", treeHumpCamelFunc, "Tree Hump Camel", {-5, 5}, 100000);
+    printResults(simulated_annealing, "Simulated Annealing", boothFunc, "Booth", {-10, 10}, 1000000);
+    printResults(simulated_annealing, "Simulated Annealing", holderTableFunc, "Holder table", {-10, 10}, 1000000);
+    printResults(simulated_annealing, "Simulated Annealing", treeHumpCamelFunc, "Tree Hump Camel", {-5, 5}, 1000000);
 
 
     return 0;
