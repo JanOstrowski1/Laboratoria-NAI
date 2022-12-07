2fe3a558ce42c0d16794d53cdeae0757476be557
Jan Ostrowski <s23820@pjwstk.edu.pl>
2022-12-07 16:25:14 +0100

    lab 8 in progress

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2\opencv.hpp>
 #include <iostream>
 
 int main(int argc, char** argv)
@@ -13,10 +14,23 @@ int main(int argc, char** argv)
     int i = 0;
     while (waitKey(10) != 27) {
         vc >> frame;
-        std::cout << "frame info: " << frame.rows << " " << frame.cols << std::endl;
+//        std::cout << "frame info: " << frame.rows << " " << frame.cols << std::endl;
 //        imwrite("frame" + std::to_string(i++) + ".png", frame);
-        cv::flip(frame, frame, 1);
+//        cv::flip(frame, frame, 1);
+
+        // Convert input image to HSV
+        cv::cvtColor(frame, frame, cv::COLOR_BGR2HSV);
+        MatExpr maxElement= cv::max(frame,cv::COLOR_BGR2HSV);
+
+        Point p1(0, 0), p2(100, 0);
+        Point p3(200, 0), p4(500, 500);
+        int thickness = 2;
+        line(frame, p1, p3, Scalar(255, 0, 0),
+             thickness, LINE_8);
         cv::imshow("Frame", frame);
+
+
+
 
     }
     return 0;
